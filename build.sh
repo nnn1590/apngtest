7bd9c952a8eb484a8cb2e7894c1c83eb678c575d
nnn1590 <28985763+nnn1590@users.noreply.github.com>
2019-12-17 17:19:15 +0900

    Add files

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+#  Copyright 2019 NNN1590
+#
+#  This file is part of apngtest.
+#
+#  apngtest is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  apngtest is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with apngtest.  If not, see <https://www.gnu.org/licenses/>.
+
+apngasm output.png 0001.png 1 5
